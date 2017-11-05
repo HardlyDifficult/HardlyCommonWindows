c7e0c8ab9d2248efe777b6fa5234440f983039b0
HardlyDifficult <HardlyDifficultLLC@gmail.com>
2017-11-05 05:25:29 -0800

    v1

---
+++ IdleChecker.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace HD
+{
+  internal struct LASTINPUTINFO
+  {
+    public uint cbSize;
+    public uint dwTime;
+  }
+
+  /// <summary>
+  /// Helps to find the idle time, (in milliseconds) spent since the last user input
+  /// </summary>
+  public class IdleTimeFinder
+  {
+    [DllImport("User32.dll")]
+    static extern bool GetLastInputInfo(ref LASTINPUTINFO plii);
+
+    [DllImport("Kernel32.dll")]
+    static extern uint GetLastError();
+    
+    public static TimeSpan GetIdleTime()
+    {
+      LASTINPUTINFO lastInPut = new LASTINPUTINFO();
+      lastInPut.cbSize = (uint)Marshal.SizeOf(lastInPut);
+      GetLastInputInfo(ref lastInPut);
+
+      return TimeSpan.FromMilliseconds(((uint)Environment.TickCount - lastInPut.dwTime));
+    }
+
+    /// <summary>
+    /// Get the Last input time in milliseconds
+    /// </summary>
+    static long GetLastInputTime()
+    {
+      LASTINPUTINFO lastInPut = new LASTINPUTINFO();
+      lastInPut.cbSize = (uint)Marshal.SizeOf(lastInPut);
+      if (!GetLastInputInfo(ref lastInPut))
+      {
+        throw new Exception(GetLastError().ToString());
+      }
+      return lastInPut.dwTime;
+    }
+  }
+}
---
+++ StartWithWindows.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Reflection;
+using System.Windows;
+
+namespace HD
+{
+  public static class StartWithWindows
+  {
+    // The path to the key where Windows looks for startup applications
+    static readonly RegistryKey rkApp = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+
+    const string appName = "Hardly Difficult Miner";
+
+    public static bool shouldStartWithWindows
+    {
+      get
+      {
+        return rkApp.GetValue(appName) != null;
+      }
+      set
+      {
+        if (value)
+        {
+          Console.WriteLine(Assembly.GetExecutingAssembly().Location); // TODO remove
+          rkApp.SetValue(appName, Assembly.GetExecutingAssembly().Location);
+        }
+        else
+        {
+          rkApp.DeleteValue(appName);
+        }
+      }
+    }
+  }
+}
